d9bae4776d63b7f99dfa06e860a185c44dca1cbe
TheGreanestOfBeans <60801727+TheGreanestOfBeans@users.noreply.github.com>
2022-12-07 15:18:20 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-echo hi
+
+sudo apt install -y boxes libnotify-bin sl
+
+echo "You've Been Trolled" | boxes
+
+sl
+
+notify-send "You've been Trolled!" && notify-send "You've been Trolled!" && notify-send "You have probably been told." && notify-send "Don't reply, to this guy;" && notify-send "he is just getting a rise, out of you!" && notify-send "Yes it's true," && notify-send "you respond and thats his cue to" && notify-send "start trouble on the double while he strokes his manly stubble" && notify-send "
+
+You've been Trolled! You've been Trolled!" && notify-send "You should probably just fold" && notify-send "When the only winning move is not to play And yet you keep on trying, Mindlessly Replying." && notify-send "You've been Trolled. You've been Trolled. Have a Nice Day!"
